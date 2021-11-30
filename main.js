1cd2f7711e82e6fd3952165b811cbf9a7f97bf8e
Arunangshu009 <83114829+Arunangshu009@users.noreply.github.com>
2021-11-30 19:21:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+img="";
+status="";
+
+function preload(){
+    img= loadImage("India.jpg");
+}
+
+function setup(){
+    canvas= createCanvas(640, 420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML= "Status : Detecting Object";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded")
+    status=true;
+
+}
+
+function gotResult(error, results){
+if(error){
+    console.log("error")
+}
+    console.log("results")
+}
+
+function draw(){
+    fill("#ffffff");
+    text("INDIA", 320, 120);
+    noFill();
+    stroke("#ff0000");
+    rect(300, 90, 270, 320);
+}
+
+function next(){
+    img= loadImage("contry.jpg");
+}
+
+function previous(){
+    img= loadImage("India.jpg");
+}
